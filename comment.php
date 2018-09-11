c1188a29a29080c80acee073961d792da87b36bc
Nikunj19618 <43145872+Nikunj19618@users.noreply.github.com>
2018-09-11 22:40:36 +0530

    Add files via upload

---
+++ comment.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Comment</title>
+</head>
+<body>
+<h1> DO comment</h1>
+<br>
+ <form method="get" action="comment.php">	
+<input type="number" name='uid' value="1">
+<textarea name="m" rows="5" cols="40"></textarea>
+<button name="submit" type="submit">Comment</button>
+</form>
+</body>
+</html>
+<?php
+$con=mysqli_connect('localhost','root','','smss');
+if(isset($_GET['submit']))
+{
+	
+	$message = $_GET['m'];
+	$uid=$_GET['uid'];
+$qry="INSERT INTO `comments`(`uid`,`message`) VALUES ('$uid','$message')";
+$run=mysqli_query($con,$qry);
+}
+?>
